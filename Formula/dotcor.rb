7100c95a84751fc7fdd7b9425bf516e0611c5662
Justin Cordova <justinavodroc@gmail.com>
2026-02-18 22:06:25 -0500

    dotcor 1.0.0

--- Formula/dotcor.rb
+++ Formula/dotcor.rb
@@ -2,7 +2,7 @@ class Dotcor < Formula
   desc "Symlink-based dotfile manager with Git integration"
   homepage "https://github.com/justincordova/dotcor"
   url "https://github.com/justincordova/dotcor/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "auto-generated"
+  sha256 "36ed03406dd66bab774fb43859b83b2eb51e288a99fd0896fa998c8ec60e06eb"
   license "MIT"
 
   depends_on "git"
