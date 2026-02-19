76bb03657700a6474af56159e963eaa49d2c3bbd
Justin Cordova <justinavodroc@gmail.com>
2026-02-18 21:44:57 -0500

    dotcor 1.0.0

--- Formula/dotcor.rb
+++ Formula/dotcor.rb
@@ -1,15 +1,14 @@
 class Dotcor < Formula
   desc "Symlink-based dotfile manager with Git integration"
   homepage "https://github.com/justincordova/dotcor"
-  url "https://github.com/justincordova/dotcor/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "943890a1abd62411d7b1ad3baa4b5b6011b6ebf1c2c36c137d3496d1af95c0b3"
+  url "https://github.com/justincordova/dotcor/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "auto-generated"
   license "MIT"
-  head "https://github.com/justincordova/dotcor.git", branch: "main"
 
-  depends_on "go" => :build
+  depends_on "git"
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/dotcor"
+    bin.install "dotcor"
   end
 
   test do
