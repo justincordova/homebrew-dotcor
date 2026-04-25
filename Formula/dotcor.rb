586a1221f8c686b3ac778e2500aca54a9341a70c
Justin Cordova <justinavodroc@gmail.com>
2026-04-25 10:13:39 -0400

    dotcor 2.0.0

--- Formula/dotcor.rb
+++ Formula/dotcor.rb
@@ -1,14 +1,18 @@
 class Dotcor < Formula
   desc "Symlink-based dotfile manager with Git integration"
   homepage "https://github.com/justincordova/dotcor"
-  version "1.0.3"
-  license "MIT"
+  version "2.0.0"
 
   depends_on "git"
 
   on_arm do
-    url "https://github.com/justincordova/dotcor/releases/download/v1.0.3/dotcor_1.0.3_darwin_arm64.tar.gz"
-    sha256 "d761bd735237232252b27e0e3105c11b8ddc57e9840cb01d933f268d97b9c930"
+    url "https://github.com/justincordova/dotcor/releases/download/v2.0.0/dotcor_2.0.0_darwin_arm64.tar.gz"
+    sha256 "af249a5573b42de6a0d79e7c6e0b799bc74c687369d4c3898a6d029a384c0356"
+  end
+
+  on_intel do
+    url "https://github.com/justincordova/dotcor/releases/download/v2.0.0/dotcor_2.0.0_darwin_amd64.tar.gz"
+    sha256 "c864809745cc5da1c0b42f2042b47b74f6fc3f49354792ef82d44b791a29e8e0"
   end
 
   on_intel do
@@ -21,6 +25,6 @@ class Dotcor < Formula
   end
 
   test do
-    assert_match "dotcor version #{version}", shell_output("#{bin}/dotcor --version")
+    assert_match "dotcor #{version}", shell_output("#{bin}/dotcor --version")
   end
 end
