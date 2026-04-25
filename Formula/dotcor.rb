6f32173538b0691d1ec2e0e7294e0d3e2f0ba695
Justin Cordova <justinavodroc@gmail.com>
2026-04-25 12:55:12 -0400

    dotcor 2.0.1

--- Formula/dotcor.rb
+++ Formula/dotcor.rb
@@ -1,23 +1,18 @@
 class Dotcor < Formula
   desc "Symlink-based dotfile manager with Git integration"
   homepage "https://github.com/justincordova/dotcor"
-  version "2.0.0"
+  version "2.0.1"
 
   depends_on "git"
 
   on_arm do
-    url "https://github.com/justincordova/dotcor/releases/download/v2.0.0/dotcor_2.0.0_darwin_arm64.tar.gz"
-    sha256 "af249a5573b42de6a0d79e7c6e0b799bc74c687369d4c3898a6d029a384c0356"
+    url "https://github.com/justincordova/dotcor/releases/download/v2.0.1/dotcor_2.0.1_darwin_arm64.tar.gz"
+    sha256 "1e4cfb25d01e3d655b446e34662660c1f912de10376288910a7deefe3b6fe191"
   end
 
   on_intel do
-    url "https://github.com/justincordova/dotcor/releases/download/v2.0.0/dotcor_2.0.0_darwin_amd64.tar.gz"
-    sha256 "c864809745cc5da1c0b42f2042b47b74f6fc3f49354792ef82d44b791a29e8e0"
-  end
-
-  on_intel do
-    url "https://github.com/justincordova/dotcor/releases/download/v1.0.3/dotcor_1.0.3_darwin_amd64.tar.gz"
-    sha256 "b1a8bcf0c6959c7b651186a2d7592326f804f0509dbd0f58dcc161608325c982"
+    url "https://github.com/justincordova/dotcor/releases/download/v2.0.1/dotcor_2.0.1_darwin_amd64.tar.gz"
+    sha256 "8520fcbf05f2f135efd990401ce94e6bcdba29d30bd2d74a395cef3f98f9b21a"
   end
 
   def install
